613e6c5dda79ca4fd4fc6577d44761377c54786f
james-fooooo <james.foo@techjourney.org>
2021-07-22 19:14:38 +0000

    fourth

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,15 @@ import { getStaticPokemonByGen } from '../lib/utils/static-utils';
 import { Layout } from "../components/Layout"
 import { Card } from "../components/Card"
 import { PokemonCards } from "../components/PokemonCards"
+import { useDeck } from "../lib/use-deck"
 import classes from '../styles/Home.module.css';
 
 
 export const getStaticProps = getStaticPokemonByGen(1);
 
 export default function Home({ pokemon }) {
-  // const { deck, clearDeck, downloadDeck, randomizeDeck, fetchDeck, toggleHandler, isSelected } = useDeck();
+  const { deck, clearDeck, downloadDeck, randomizeDeck, fetchDeck, toggleHandler, isSelected } = useDeck();
+  
   return (
     <Layout title="Home">
       <div></div>
